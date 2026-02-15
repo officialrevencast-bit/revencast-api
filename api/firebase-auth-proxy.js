06e8def217cc18da3b45772ee8fdcea93c5b1239
officialrevencast-bit <officialrevencast@gmail.com>
2026-02-15 16:47:01 +0400

    Update firebase-auth-proxy.js

--- api/firebase-auth-proxy.js
+++ api/firebase-auth-proxy.js
@@ -1,11 +1,7 @@
 export default async function handler(req, res) {
   const internalSecret = process.env.INTERNAL_PROXY_SECRET;
   const incomingSecret = req.headers['x-internal-secret'];
-  const licenseKey = req.headers['x-license-key'];
-  const expectedLicense = process.env.REVENCAST_API_KEY;
-  const hasInternalAuth = !!internalSecret && incomingSecret === internalSecret;
-  const hasLegacyAuth = !!expectedLicense && licenseKey === expectedLicense;
-  if (!hasInternalAuth && !hasLegacyAuth) {
+  if (!internalSecret || incomingSecret !== internalSecret) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
 
