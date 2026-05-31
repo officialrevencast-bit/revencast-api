8a5fb8d68c80e7d0ea44f79dffb8f1fad969fc9e
officialrevencast-bit <officialrevencast@gmail.com>
2026-05-31 21:30:32 +0400

    Update serpapi-proxy.js

--- api/serpapi-proxy.js
+++ api/serpapi-proxy.js
@@ -18,7 +18,8 @@ export default async function handler(req, res) {
   const apiKeys = [
     process.env.SERPAPI_KEY,
     process.env.SERPAPI_KEY_2,
-    process.env.SERPAPI_KEY_3
+    process.env.SERPAPI_KEY_3,
+    process.env.SERPAPI_KEY_4,
   ].filter(Boolean);
 
   if (apiKeys.length === 0) {
